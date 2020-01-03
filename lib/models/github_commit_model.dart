0bdf7126347f4a712b205f21fc52e4249f210270
Mark Diesta <markdiesta@@gmail.com>
2020-01-04 04:17:26 +0800

    Reduced needed fields in the GitHubCommitModel.

--- lib/models/github_commit_model.dart
+++ lib/models/github_commit_model.dart
@@ -12,136 +12,44 @@ String gitHubCommitToJson(List<GitHubCommitModel> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class GitHubCommitModel {
-  String sha;
-  String nodeId;
   Commit commit;
-  String url;
-  String htmlUrl;
-  String commentsUrl;
   GitHubCommitAuthor author;
   GitHubCommitAuthor committer;
-  List<dynamic> parents;
 
   GitHubCommitModel({
-    this.sha,
-    this.nodeId,
     this.commit,
-    this.url,
-    this.htmlUrl,
-    this.commentsUrl,
     this.author,
     this.committer,
-    this.parents,
   });
 
   factory GitHubCommitModel.fromJson(Map<String, dynamic> json) =>
       GitHubCommitModel(
-        sha: json["sha"],
-        nodeId: json["node_id"],
         commit: Commit.fromJson(json["commit"]),
-        url: json["url"],
-        htmlUrl: json["html_url"],
-        commentsUrl: json["comments_url"],
         author: GitHubCommitAuthor.fromJson(json["author"]),
         committer: GitHubCommitAuthor.fromJson(json["committer"]),
-        parents: List<dynamic>.from(json["parents"].map((x) => x)),
       );
 
   Map<String, dynamic> toJson() => {
-        "sha": sha,
-        "node_id": nodeId,
         "commit": commit.toJson(),
-        "url": url,
-        "html_url": htmlUrl,
-        "comments_url": commentsUrl,
         "author": author.toJson(),
         "committer": committer.toJson(),
-        "parents": List<dynamic>.from(parents.map((x) => x)),
       };
 }
 
 class GitHubCommitAuthor {
-  String login;
-  int id;
-  String nodeId;
   String avatarUrl;
-  String gravatarId;
-  String url;
-  String htmlUrl;
-  String followersUrl;
-  String followingUrl;
-  String gistsUrl;
-  String starredUrl;
-  String subscriptionsUrl;
-  String organizationsUrl;
-  String reposUrl;
-  String eventsUrl;
-  String receivedEventsUrl;
-  String type;
-  bool siteAdmin;
 
   GitHubCommitAuthor({
-    this.login,
-    this.id,
-    this.nodeId,
     this.avatarUrl,
-    this.gravatarId,
-    this.url,
-    this.htmlUrl,
-    this.followersUrl,
-    this.followingUrl,
-    this.gistsUrl,
-    this.starredUrl,
-    this.subscriptionsUrl,
-    this.organizationsUrl,
-    this.reposUrl,
-    this.eventsUrl,
-    this.receivedEventsUrl,
-    this.type,
-    this.siteAdmin,
   });
 
   factory GitHubCommitAuthor.fromJson(Map<String, dynamic> json) =>
       GitHubCommitAuthor(
-        login: json["login"],
-        id: json["id"],
-        nodeId: json["node_id"],
         avatarUrl: json["avatar_url"],
-        gravatarId: json["gravatar_id"],
-        url: json["url"],
-        htmlUrl: json["html_url"],
-        followersUrl: json["followers_url"],
-        followingUrl: json["following_url"],
-        gistsUrl: json["gists_url"],
-        starredUrl: json["starred_url"],
-        subscriptionsUrl: json["subscriptions_url"],
-        organizationsUrl: json["organizations_url"],
-        reposUrl: json["repos_url"],
-        eventsUrl: json["events_url"],
-        receivedEventsUrl: json["received_events_url"],
-        type: json["type"],
-        siteAdmin: json["site_admin"],
       );
 
   Map<String, dynamic> toJson() => {
-        "login": login,
-        "id": id,
-        "node_id": nodeId,
         "avatar_url": avatarUrl,
-        "gravatar_id": gravatarId,
-        "url": url,
-        "html_url": htmlUrl,
-        "followers_url": followersUrl,
-        "following_url": followingUrl,
-        "gists_url": gistsUrl,
-        "starred_url": starredUrl,
-        "subscriptions_url": subscriptionsUrl,
-        "organizations_url": organizationsUrl,
-        "repos_url": reposUrl,
-        "events_url": eventsUrl,
-        "received_events_url": receivedEventsUrl,
-        "type": type,
-        "site_admin": siteAdmin,
       };
 }
 
@@ -149,39 +57,23 @@ class Commit {
   CommitAuthor author;
   CommitAuthor committer;
   String message;
-  Tree tree;
-  String url;
-  int commentCount;
-  Verification verification;
 
   Commit({
     this.author,
     this.committer,
     this.message,
-    this.tree,
-    this.url,
-    this.commentCount,
-    this.verification,
   });
 
   factory Commit.fromJson(Map<String, dynamic> json) => Commit(
         author: CommitAuthor.fromJson(json["author"]),
         committer: CommitAuthor.fromJson(json["committer"]),
         message: json["message"],
-        tree: Tree.fromJson(json["tree"]),
-        url: json["url"],
-        commentCount: json["comment_count"],
-        verification: Verification.fromJson(json["verification"]),
       );
 
   Map<String, dynamic> toJson() => {
         "author": author.toJson(),
         "committer": committer.toJson(),
         "message": message,
-        "tree": tree.toJson(),
-        "url": url,
-        "comment_count": commentCount,
-        "verification": verification.toJson(),
       };
 }
 
@@ -209,50 +101,3 @@ class CommitAuthor {
       };
 }
 
-class Tree {
-  String sha;
-  String url;
-
-  Tree({
-    this.sha,
-    this.url,
-  });
-
-  factory Tree.fromJson(Map<String, dynamic> json) => Tree(
-        sha: json["sha"],
-        url: json["url"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "sha": sha,
-        "url": url,
-      };
-}
-
-class Verification {
-  bool verified;
-  String reason;
-  dynamic signature;
-  dynamic payload;
-
-  Verification({
-    this.verified,
-    this.reason,
-    this.signature,
-    this.payload,
-  });
-
-  factory Verification.fromJson(Map<String, dynamic> json) => Verification(
-        verified: json["verified"],
-        reason: json["reason"],
-        signature: json["signature"],
-        payload: json["payload"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "verified": verified,
-        "reason": reason,
-        "signature": signature,
-        "payload": payload,
-      };
-}
