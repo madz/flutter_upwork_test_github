aa652a4141c5ffa78eb85bd7b0187ea3d3c78d07
madz <markdiesta@gmail.com>
2020-01-04 03:43:41 +0800

    Fixed URL

--- lib/constants/github_constants.dart
+++ lib/constants/github_constants.dart
@@ -2,9 +2,9 @@ class GithubConstants {
   static String baseGithubUrl = "https://api.github.com";
 
   static String user = "madz";
-  static String $ = "flutter_upwork_test_github";
+  static String repo = "flutter_upwork_test_github";
 
   static String getUser = "$baseGithubUrl/users$user";
   static String getUserRepos = "$baseGithubUrl/users/$user/repos";
-  static String getUserCommits = "$baseGithubUrl/repos/$user/$user/commits";
+  static String getUserCommits = "$baseGithubUrl/repos/$user/$repo/commits";
 }
